c4f6511d9c418ded22fc40053e8ccf0392d234a5
Dan <daniel.parton@hotmail.com>
2023-09-10 14:30:33 +0800

    FIX: Github actions

--- scripts/test.ps1
+++
@@ -1,9 +0,0 @@
-$srcPath = Join-Path -Path $PSScriptRoot -ChildPath "..\src"
-$testsToRun = Get-ChildItem -Path $srcPath -Recurse -File -Filter *.Test.csproj
-
-foreach ($testToRun in $testsToRun) {
-  dotnet test $testToRun.FullName --no-restore -c "Release"
-  if ($LASTEXITCODE -ne 0) { $failure = $true }
-}
-
-if ($testFailure -eq $true) { throw "broken tests" }
